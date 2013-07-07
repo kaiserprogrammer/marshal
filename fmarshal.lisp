026c3f5a97d1bfc268dad1fad67f74307b663c7c
Jürgen Bickert <juergenbickert@gmail.com>
2013-07-08 00:46:04 +0200

    use streams instead of strings

--- fmarshal.lisp
+++ fmarshal.lisp
@@ -9,58 +9,83 @@
   (let ((g (gensym)))
     `(let ((,g ,o))
        (if (gethash ,g *refs*)
-           (dump-ref (gethash ,g *refs*))
+           (dump-ref (gethash ,g *refs*) stream)
            (progn (setf (gethash ,g *refs*) (next-ref))
                   ,@body)))))
 
 (defvar *refs*)
 (defvar *next-ref*)
 
-(defun dump (object)
+(defun dump (object &optional stream)
   (let ((*refs* (make-hash-table))
         (*next-ref* 0))
-    (dump-helper object)))
+    (if stream
+        (dump-helper object stream)
+        (with-output-to-string (stream)
+          (dump-helper object stream)))))
 
-(defgeneric dump-helper (object))
-(defmethod dump-helper ((s string))
-  (format nil "~w" s))
-(defmethod dump-helper ((c character))
-  (format nil "~w" c))
-(defmethod dump-helper ((n number))
-  (format nil "~w" n))
-(defmethod dump-helper ((l cons))
+(defgeneric dump-helper (object stream))
+(defmethod dump-helper ((s string) stream)
+  (format stream "~w" s))
+(defmethod dump-helper ((c character) stream)
+  (format stream "~w" c))
+(defmethod dump-helper ((n number) stream)
+  (format stream "~w" n))
+(defmethod dump-helper ((l cons) stream)
   (maybe-dump-ref l
-   (format nil "(fmarshal::dumped :list ~a ~a)" (gethash l *refs*) (mapcar #'dump-helper l))))
-(defmethod dump-helper ((a array))
-  (maybe-dump-ref a
-    (format nil "(fmarshal::dumped :array ~a ~a ~a)" (gethash a *refs*) (array-dimensions a) (dump-array a))))
-(defmethod dump-helper ((h hash-table))
-  (maybe-dump-ref h
-    (format nil "(fmarshal::dumped :hash-table ~a ~a ~a)" (gethash h *refs*) (hash-table-test h) (dump-hash-table h))))
+    (progn
+      (format stream "(fmarshal::dumped :list ~a (" (gethash l *refs*))
+      (dolist (e l)
+        (write-string " " stream)
+        (dump-helper e stream))
+      (write-string ")" stream)
+      (write-string ")" stream))))
 
-(defmethod dump-helper ((s symbol))
-  (format nil "~s" s))
+(defmethod dump-helper ((a array) stream)
+  (maybe-dump-ref a
+    (progn
+      (format stream "(fmarshal::dumped :array ~a ~a " (gethash a *refs*) (array-dimensions a))
+      (dump-array a stream)
+      (write-string "closear)" stream))))
+
+(defmethod dump-helper ((h hash-table) stream)
+  (maybe-dump-ref h
+    (progn
+      (write-string "(fmarshal::dumped :hash-table " stream)
+      (format stream "~a ~a " (gethash h *refs*) (hash-table-test h))
+      (dump-hash-table h stream)
+      (write-string ")" stream))))
+
+(defmethod dump-helper ((s symbol) stream)
+  (format stream "~s" s))
 
 (defun next-ref ()
   (incf *next-ref*))
 
-(defmethod dump-helper (object)
+(defmethod dump-helper (object stream)
   (maybe-dump-ref object
-    (format nil "(fmarshal::dumped ~a ~s ~a)" (gethash object *refs*) (class-name (class-of object)) (dump-slot-definitions object))))
+    (progn
+      (write-string "(fmarshal::dumped " stream)
+      (format stream "~a " (gethash object *refs*))
+      (format stream "~s " (class-name (class-of object)))
+      (dump-slot-definitions object stream)
+      (write-string ")" stream))))
 
-(defun dump-ref (ref)
-  (format nil "(fmarshal::dumped :ref ~a)" ref))
+(defun dump-ref (ref stream)
+  (format stream "(fmarshal::dumped :ref ~a)" ref))
 
-(defun dump-array (a)
-  (dump-array-helper (array-dimensions a) a nil))
+(defun dump-array (a stream)
+  (dump-array-helper (array-dimensions a) a nil stream))
 
-(defun dump-array-helper (dim a ref)
+(defun dump-array-helper (dim a ref stream)
   (if (null dim)
-      (dump-helper (apply #'aref a (reverse ref)))
-      (let ((list (list)))
+      (dump-helper (apply #'aref a (reverse ref)) stream)
+      (progn
+        (write-string "(" stream)
         (dotimes (i (car dim))
-          (push (dump-array-helper (cdr dim) a (cons i ref)) list))
-        (nreverse list))))
+          (write-string " " stream)
+          (dump-array-helper (cdr dim) a (cons i ref) stream))
+        (write-string ")" stream))))
 
 (defun load-array (ref dim list)
   (let ((a (make-array dim)))
@@ -75,10 +100,16 @@
          for e in list
          do (load-array-helper (load-helper e) a (cons i ref)))))
 
-(defun dump-hash-table (h)
+(defun dump-hash-table (h stream)
+  (write-string "(" stream)
   (loop for v being the hash-values in h using (hash-key k)
-     collect (cons (dump-helper k)
-                   (dump-helper v))))
+     do (progn
+          (write-string "(" stream)
+          (dump-helper k stream)
+          (write-string " . " stream)
+          (dump-helper v stream)
+          (write-string ")" stream)))
+  (write-string ")" stream))
 
 (defun load (stream)
   (when stream
@@ -126,11 +157,13 @@
        do (setf (gethash (load-helper k) h) (load-helper v)))
     h))
 
-(defun dump-slot-definitions (object)
-  (with-output-to-string (str)
-    (write-string "(" str)
-    (let ((slots (mapcar #'closer-mop:slot-definition-name (closer-mop:class-slots (class-of object)))))
-      (loop for slot in slots
-         when (slot-boundp object slot)
-         do (format str "(~s . ~a)" slot (dump-helper (slot-value object slot)))))
-    (write-string ")" str)))
+(defun dump-slot-definitions (object stream)
+  (write-string "(" stream)
+  (let ((slots (mapcar #'closer-mop:slot-definition-name (closer-mop:class-slots (class-of object)))))
+    (loop for slot in slots
+       when (slot-boundp object slot)
+       do (progn
+            (format stream "(~s . " slot)
+            (dump-helper (slot-value object slot) stream)
+            (write-string ")" stream))))
+  (write-string ")" stream))
