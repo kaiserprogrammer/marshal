d41c4fe39b2664e13bccd27b7145d2ca7b6b37fa
Jürgen Bickert <juergenbickert@gmail.com>
2013-07-20 06:09:02 +0200

    simplify dump and load to use less space

--- fmarshal.lisp
+++ fmarshal.lisp
@@ -34,7 +34,7 @@
 (defmethod dump-helper ((l cons) stream)
   (maybe-dump-ref l
     (progn
-      (format stream "(fmarshal::dumped :list ~a (" (gethash l *refs*))
+      (write-string "(" stream)
       (do ((e l (cdr e)))
           ((or (atom e) (null e)))
         (write-string " " stream)
@@ -44,21 +44,19 @@
                    (write-string " . " stream)
                    (dump-helper (cdr e) stream))
             (dump-helper (car e) stream)))
-      (write-string ")" stream)
       (write-string ")" stream))))
 
 (defmethod dump-helper ((a array) stream)
   (maybe-dump-ref a
     (progn
-      (format stream "(fmarshal::dumped :array ~a ~a " (gethash a *refs*) (array-dimensions a))
-      (dump-array a stream)
-      (write-string ")" stream))))
+      (format stream "#~aA" (length (array-dimensions a)))
+      (dump-array a stream))))
 
 (defmethod dump-helper ((h hash-table) stream)
   (maybe-dump-ref h
     (progn
-      (write-string "(fmarshal::dumped :hash-table " stream)
-      (format stream "~a ~a " (gethash h *refs*) (hash-table-test h))
+      (write-string "(:fm :hash-table " stream)
+      (format stream "~a " (hash-table-test h))
       (dump-hash-table h stream)
       (write-string ")" stream))))
 
@@ -71,14 +69,13 @@
 (defmethod dump-helper (object stream)
   (maybe-dump-ref object
     (progn
-      (write-string "(fmarshal::dumped " stream)
-      (format stream "~a " (gethash object *refs*))
+      (write-string "(:fm " stream)
       (format stream "~s " (class-name (class-of object)))
       (dump-slot-definitions object stream)
       (write-string ")" stream))))
 
 (defun dump-ref (ref stream)
-  (format stream "(fmarshal::dumped :ref ~a)" ref))
+  (format stream "(:fm :ref ~a)" ref))
 
 (defun dump-array (a stream)
   (dump-array-helper (array-dimensions a) a nil stream))
@@ -93,18 +90,17 @@
           (dump-array-helper (cdr dim) a (cons i ref) stream))
         (write-string ")" stream))))
 
-(defun load-array (ref dim list)
-  (let ((a (make-array dim)))
-    (setf (gethash ref *refs*) a)
-    (load-array-helper list a (list) dim)
-    a))
+(defun load-array (a)
+  (setf (gethash (next-ref) *refs*) a)
+  (load-array-helper a (list) (array-dimensions a))
+  a)
 
-(defun load-array-helper (list a ref dim)
+(defun load-array-helper (a ref dim)
   (if (null dim)
-      (setf (apply #'aref a (reverse ref)) list)
-      (loop for i from 0 below (car dim)
-         for e in list
-         do (load-array-helper (load-helper e) a (cons i ref) (cdr dim)))))
+      (unless (atom (apply #'aref a (reverse ref)))
+        (setf (apply #'aref a (reverse ref)) (load-helper (apply #'aref a (reverse ref)))))
+      (loop for i from 0 below (car (last dim))
+         do (load-array-helper a (append ref (list i)) (butlast dim)))))
 
 (defun dump-hash-table (h stream)
   (write-string "(" stream)
@@ -119,7 +115,8 @@
 
 (defun load (stream)
   (when stream
-    (let ((*refs* (make-hash-table)))
+    (let ((*refs* (make-hash-table))
+          (*next-ref* 0))
       (unmarshalize stream))))
 
 (defun unmarshalize (stream)
@@ -130,40 +127,38 @@
 
 
 (defun load-helper (desc)
-  (if (or (not (consp desc)) (not (eq 'fmarshal::dumped (first desc))))
-      (if (consp desc)
-          (mapcar #'load-helper desc)
-          desc)
-      (if (eq :list (second desc))
-          (load-list (third desc) (fourth desc))
-          (if (eq :hash-table (second desc))
-              (load-hash-table (third desc) (fourth desc) (fifth desc))
-              (if (eq :array (second desc))
-                  (load-array (third desc) (fourth desc) (fifth desc))
+  (if (arrayp desc)
+      (load-array desc)
+      (if (atom desc)
+          desc
+          (if (not (eq :fm (first desc)))
+              (load-list desc)
+              (if (eq :hash-table (second desc))
+                  (load-hash-table (third desc) (fourth desc))
                   (if (eq :ref (second desc))
                       (gethash (third desc) *refs*)
-                      (load-object (second desc) (third desc) (fourth desc))))))))
+                      (load-object (second desc) (third desc))))))))
 
-(defun load-list (ref list)
+(defun load-list (list)
+  (setf (gethash (next-ref) *refs*) list)
   (do ((e list (cdr e)))
       ((or (atom e) (null e)))
     (if (atom (cdr e))
         (progn (setf (car e) (load-helper (car e)))
                (setf (cdr e) (load-helper (cdr e))))
         (setf (car e) (load-helper (car e)))))
-  (setf (gethash ref *refs*) list)
   list)
 
-(defun load-object (ref class slots-desc)
+(defun load-object (class slots-desc)
   (let ((o (make-instance class)))
-    (setf (gethash ref *refs*) o)
+    (setf (gethash (next-ref) *refs*) o)
     (loop for (slot-name . slot-value) in slots-desc
        do (setf (slot-value o slot-name) (load-helper slot-value)))
     o))
 
-(defun load-hash-table (ref test desc)
+(defun load-hash-table (test desc)
   (let ((h (make-hash-table :test test)))
-    (setf (gethash ref *refs*) h)
+    (setf (gethash (next-ref) *refs*) h)
     (loop for (k . v) in desc
        do (setf (gethash (load-helper k) h) (load-helper v)))
     h))
